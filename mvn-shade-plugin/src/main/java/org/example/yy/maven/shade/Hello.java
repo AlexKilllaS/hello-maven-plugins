b4205e8be6aadfd5ca20440bd432b2caac0cfb9f
yyhuangfu <hfyy0726@outlook.com>
2024-01-13 22:56:35 +0800

    init

--- mvn-shade-plugin/src/main/java/org/example/yy/maven/shade/Hello.java
+++ mvn-shade-plugin/src/main/java/org/example/yy/maven/shade/Hello.java
@@ -1,2 +1,15 @@
-package org.example.yy.maven.shade;public class Hello {
+package org.example.yy.maven.shade;
+
+import cn.hutool.core.util.RandomUtil;
+
+public class Hello {
+    public void say() {
+        int[] randomInt = RandomUtil.randomInts(5);
+        System.out.printf("hello, random:" + randomInt);
+    }
+
+    // mvn-shade-plugin-1.0-SNAPSHOT.jar                   肥包
+    // mvn-shade-plugin-1.0-SNAPSHOT-sources.jar           肥包源码
+    // original-mvn-shade-plugin-1.0-SNAPSHOT.jar          原始小包
+
 }
