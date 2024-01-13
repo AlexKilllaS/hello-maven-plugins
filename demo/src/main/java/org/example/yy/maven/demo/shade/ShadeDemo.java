50cf1a7938d0da6f6b69ddfa1f68be125414fc47
yyhuangfu <hfyy0726@outlook.com>
2024-01-13 22:56:35 +0800

    init

--- demo/src/main/java/org/example/yy/maven/demo/shade/ShadeDemo.java
+++ demo/src/main/java/org/example/yy/maven/demo/shade/ShadeDemo.java
@@ -1,2 +1,10 @@
-package org.example.yy.maven.demo.shade;public class ShadeDemo {
+package org.example.yy.maven.demo.shade;
+
+import org.example.yy.maven.shade.Hello;
+
+public class ShadeDemo {
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        hello.say();
+    }
 }
